67615e3a198d14b8539471a9a092217ee11fcea3
Hack Reactor Students <github@hackreactor.com>
2013-11-25 16:19:28 -0800

    workingin functional. gonna add a Message class

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,49 +1,42 @@
-var ChatterBox = function(){
-  this.userName=''; // grab this from the prompt
-  this.listOfMessages = [];
-  this.getMessages = function(){
-    $.ajax({
-      // always use this url
-      url: 'https://api.parse.com/1/classes/chatterbox',
-      type: 'GET',
-      contentType: 'application/json',
-      success: function (data) {
+
+var userName=''; // grab this from the prompt
+var listOfMessages = [];
 
 
-        _.each(data.results, function(messageJSON){
-          renderMessage.call(that, messageJSON);
-          console.log('got some messages, ', messageJSON); //debugging
-        });
-      },
-      error: function (data) {
-        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to get message');
-      }
-    });
-  };
-};
-
-
-
-
-ChatterBox.prototype.printMessages = function(listOfMessages){
-  _.each(listOfMessages, function(msgNode) {
-    $('#main').append(msgNode);
+var getMessages = function(){
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      _.each(data.results, function(messageJSON){
+        renderMessage(messageJSON);
+      });
+      printMessages(listOfMessages);
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to get message');
+    }
   });
 };
 
-
-ChatterBox.prototype.renderMessage = function(messageJSON){
+var renderMessage = function(messageJSON){
   var $messageNode = $('<div></div>');
   for (var k in messageJSON) {
     var $msgPart = $('<span></span>');
     $msgPart.addClass(k);
     $messageNode.append($msgPart.text(messageJSON[k]));
   }
-  this.listOfMessages.push($messageNode);
+  listOfMessages.push($messageNode);
 };
 
-
+var printMessages = function(listOfMessages){
+  _.each(listOfMessages, function(msgNode) {
+    $('#main').append(msgNode);
+  });
+};
 
 
 
@@ -78,3 +71,25 @@ ChatterBox.prototype.renderMessage = function(messageJSON){
 
 // look @ user pages 
 //
+
+// ChatterBox.prototype.getMessages = function(){
+//   $.ajax({
+//     // always use this url
+//     url: 'https://api.parse.com/1/classes/chatterbox',
+//     type: 'GET',
+//     contentType: 'application/json',
+
+//     success: function (data) {
+//       this.renderMessage();
+//       _.each(data.results, function(messageJSON){
+//         renderMessage.call(that, messageJSON);
+//         console.log('got some messages, ', messageJSON); //debugging
+//       });
+//     },
+
+//     error: function (data) {
+//       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//       console.error('chatterbox: Failed to get message');
+//     }
+//   });
+// };
